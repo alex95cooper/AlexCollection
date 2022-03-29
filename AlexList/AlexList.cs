51dab7caaf76d269da22f8d162397b481c98accf
alex95cooper <lex95bond@gmail.com>
2022-03-29 17:29:55 +0300

    Added new coolection AlexList and method Add

--- AlexList/AlexList.cs
+++ AlexList/AlexList.cs
@@ -1,7 +1,59 @@
-﻿namespace AlexList
-{
-    public class AlexList<T>
-    {
+﻿using System.Collections;
 
+namespace AlexList
+{
+    public class AlexList<T> 
+    {
+        int indexOfElement = -1;
+
+        private T[] elementsArray;
+
+        public AlexList()
+        {
+            elementsArray = Array.Empty<T>();
+        }
+
+        #region
+
+        public bool MoveNext()
+        {
+            if (indexOfElement < elementsArray.Length - 1)
+            {
+                indexOfElement++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+
+            }
+        }
+
+        public object Current
+        {
+            get
+            {
+                return elementsArray[indexOfElement];
+            }
+        }
+
+        public void Reset()
+        {
+            indexOfElement = -1;
+        }
+
+        public AlexList<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        #endregion
+
+        public void Add(T value)
+        {
+            Array.Resize<T>(ref elementsArray, elementsArray.Length + 1);
+            elementsArray[^1] = value;
+        }
     }
 }
---
+++ TestingAlexList/Program.cs
@@ -0,0 +1,10 @@
+﻿using AlexList;
+
+AlexList<string> alexList = new();
+alexList.Add("four");
+alexList.Add("five");
+
+foreach (string i in alexList)
+    Console.WriteLine(i);
+
+Console.ReadLine();
