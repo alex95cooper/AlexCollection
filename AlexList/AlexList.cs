3f6a8a35940c3bd64fc8b8830ddd15d349a1925f
alex95cooper <lex95bond@gmail.com>
2022-04-01 11:08:45 +0300

    Added a few methods to the AlexList

--- AlexList/AlexList.cs
+++ AlexList/AlexList.cs
@@ -2,9 +2,9 @@
 
 namespace AlexList
 {
-    public class AlexList<T> 
+    public class AlexList<T>
     {
-        int indexOfElement = -1;
+        private int indexOfElement = -1;
 
         private T[] elementsArray;
 
@@ -54,6 +54,54 @@ namespace AlexList
         {
             Array.Resize<T>(ref elementsArray, elementsArray.Length + 1);
             elementsArray[^1] = value;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(elementsArray);
+        }
+
+        public void Contains()
+        {
+            
+        }
+
+        public int IndexOf(T value)
+        {
+            return Array.IndexOf(elementsArray, value);
+        }
+
+        public void Insert(T value, int index)
+        {
+            Array.Resize<T>(ref elementsArray, elementsArray.Length + 1);
+
+            for (int counter = elementsArray.Length - 2; counter >= index; counter--)
+            {
+                elementsArray[counter + 1] = elementsArray[counter];
+            }
+
+            elementsArray[index] = value;
+        }
+
+        public void Remove(T value)
+        {
+            int index = Array.IndexOf(elementsArray, value);
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int counter = index; counter < elementsArray.Length - 1; counter++)
+            {
+                elementsArray[counter] = elementsArray[counter + 1];
+            }
+
+            Array.Resize<T>(ref elementsArray, elementsArray.Length - 1);
+        }
+
+        public void Sort()
+        {
+
         }
     }
 }
--- TestingAlexList/Program.cs
+++ TestingAlexList/Program.cs
@@ -1,10 +1,21 @@
 ﻿using AlexList;
 
-AlexList<string> alexList = new();
-alexList.Add("four");
-alexList.Add("five");
+AlexList<int> alexList = new();
+alexList.Add(1);
+alexList.Add(2);
+alexList.Add(3);
+alexList.Add(4);
+alexList.Add(5);
+alexList.Add(6);
+alexList.Insert(8, 3);
 
-foreach (string i in alexList)
+
+foreach (var i in alexList)
+    Console.WriteLine(i);
+
+alexList.RemoveAt(3);
+Console.WriteLine();
+foreach (var i in alexList)
     Console.WriteLine(i);
 
 Console.ReadLine();
