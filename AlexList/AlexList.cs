57c32179cc20170032679a737d6f4a10e8559c1b
alex95cooper <lex95bond@gmail.com>
2022-04-20 13:48:24 +0300

    Renamed TestingAlexList, added _ to fields names, put private methods at the end of the class.

--- AlexList/AlexList.cs
+++ AlexList/AlexList.cs
@@ -4,22 +4,22 @@ namespace AlexList
 {
     public class AlexList<T>
     {
-        private int indexOfElement = -1;
+        private int _indexOfElement = -1;
 
-        private T[] elementsArray;
+        private T[] _elementsArray;
 
         public AlexList()
         {
-            elementsArray = new T[0];
+            _elementsArray = new T[0];
         }
 
         #region Enumerable
 
         public bool MoveNext()
         {
-            if (indexOfElement < elementsArray.Length - 1)
+            if (_indexOfElement < _elementsArray.Length - 1)
             {
-                indexOfElement++;
+                _indexOfElement++;
                 return true;
             }
             else
@@ -33,13 +33,13 @@ namespace AlexList
         {
             get
             {
-                return elementsArray[indexOfElement];
+                return _elementsArray[_indexOfElement];
             }
         }
 
         public void Reset()
         {
-            indexOfElement = -1;
+            _indexOfElement = -1;
         }
 
         public AlexList<T> GetEnumerator()
@@ -51,37 +51,138 @@ namespace AlexList
 
         public void Add(T value)
         {
-            ArrayResize(elementsArray.Length + 1);
-            elementsArray[^1] = value;
+            ArrayResize(_elementsArray.Length + 1);
+            _elementsArray[^1] = value;
+        }
+        
+        public int BinarySearch(T value)
+        {
+            int lowerRangeLimit = 0;
+            int upperRangeLimit = _elementsArray.Length - 1;
+
+            return RecursionSearch(lowerRangeLimit, upperRangeLimit, value);
+        }
+        
+        public void Clear()
+        {
+            _elementsArray = new T[0];
+        }
+
+        public bool Contains(T value)
+        {
+            if (IndexOf(value) == -1)
+                return false;
+            else
+                return true;
+        }
+
+        public int FindIndex(Predicate<T> predicate)
+        {
+            for (int counter = 0; counter < _elementsArray.Length - 1; counter++)
+            {
+                if (predicate.Invoke(_elementsArray[counter]))
+                {
+                    return counter;
+                }
+            }
+
+            return -1;
+        }
+
+        public int FindLastIndex(Predicate<T> predicate)
+        {
+            for (int counter = _elementsArray.Length - 1; counter > -1; counter--)
+            {
+                if (predicate.Invoke(_elementsArray[counter]))
+                {
+                    return counter;
+                }
+            }
+
+            return -1;
+        }
+
+        public int IndexOf(T value)
+        {
+            for (int counter = 0; counter < _elementsArray.Length - 1; counter++)
+            {
+                if (Compare(value, _elementsArray[counter]) == 0)
+                {
+                    return counter;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(T value, int index)
+        {
+            ArrayResize(_elementsArray.Length + 1);
+
+            for (int counter = _elementsArray.Length - 2; counter >= index; counter--)
+            {
+                _elementsArray[counter + 1] = _elementsArray[counter];
+            }
+
+            _elementsArray[index] = value;
+        }
+
+        public void Remove(T value)
+        {
+            int index = IndexOf(value);
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int counter = index; counter < _elementsArray.Length - 1; counter++)
+            {
+                _elementsArray[counter] = _elementsArray[counter + 1];
+            }
+
+            ArrayResize(_elementsArray.Length - 1);
+        }
+
+        public void Sort()
+        {
+            bool arrayIsNotSorted;
+
+            do
+            {
+                arrayIsNotSorted = false;
+
+                for (int counter = 0; counter < _elementsArray.Length - 1; counter++)
+                {
+                    if (Compare(_elementsArray[counter], _elementsArray[counter + 1]) > 0)
+                    {
+                        Insert(_elementsArray[counter + 1], counter);
+                        RemoveAt(counter + 2);
+                        arrayIsNotSorted = true;
+                    }
+                }
+            }
+            while (arrayIsNotSorted == true);
         }
 
         private void ArrayResize(int newSize)
         {
-            T[] interimElementsArray = new T[newSize];
+            T[] interim_elementsArray = new T[newSize];
 
-            if (elementsArray.Length > 0)
+            if (_elementsArray.Length > 0)
             {
-                if (elementsArray.Length < newSize || elementsArray.Length == newSize)
+                if (_elementsArray.Length < newSize || _elementsArray.Length == newSize)
                 {
-                    for (int counter = 0; counter < elementsArray.Length; counter++)
-                        interimElementsArray[counter] = elementsArray[counter];
+                    for (int counter = 0; counter < _elementsArray.Length; counter++)
+                        interim_elementsArray[counter] = _elementsArray[counter];
                 }
-                else if (elementsArray.Length > newSize)
+                else if (_elementsArray.Length > newSize)
                 {
                     for (int counter = 0; counter < newSize; counter++)
-                        interimElementsArray[counter] = elementsArray[counter];
-                }                
+                        interim_elementsArray[counter] = _elementsArray[counter];
+                }
             }
 
-            elementsArray = interimElementsArray;
-        }
-
-        public int BinarySearch(T value)
-        {
-            int lowerRangeLimit = 0;
-            int upperRangeLimit = elementsArray.Length - 1;
-
-            return RecursionSearch(lowerRangeLimit, upperRangeLimit, value);
+            _elementsArray = interim_elementsArray;
         }
 
         private int Compare(Object? x, Object? y)
@@ -98,32 +199,32 @@ namespace AlexList
 
             int middleOfRange = (lowerRangeLimit + (rangeOfSearching / 2));
 
-            if (Compare(value, elementsArray[lowerRangeLimit]) == 0)
+            if (Compare(value, _elementsArray[lowerRangeLimit]) == 0)
             {
                 return lowerRangeLimit;
             }
-            else if (Compare(value, elementsArray[middleOfRange]) == 0)
+            else if (Compare(value, _elementsArray[middleOfRange]) == 0)
             {
                 return middleOfRange;
             }
-            else if (Compare(value, elementsArray[upperRangeLimit]) == 0)
+            else if (Compare(value, _elementsArray[upperRangeLimit]) == 0)
             {
                 return upperRangeLimit;
             }
-            else if (Compare(value, elementsArray[lowerRangeLimit]) < 0)
+            else if (Compare(value, _elementsArray[lowerRangeLimit]) < 0)
             {
                 return -1;
             }
-            else if (Compare(value, elementsArray[upperRangeLimit]) > 0)
+            else if (Compare(value, _elementsArray[upperRangeLimit]) > 0)
             {
                 return -(upperRangeLimit + 1);
             }
-            else if (Compare(value, elementsArray[lowerRangeLimit]) > 0 && Compare(value, elementsArray[middleOfRange]) < 0)
+            else if (Compare(value, _elementsArray[lowerRangeLimit]) > 0 && Compare(value, _elementsArray[middleOfRange]) < 0)
             {
                 upperRangeLimit = middleOfRange;
                 return RecursionSearch(lowerRangeLimit, upperRangeLimit, value);
             }
-            else if (Compare(value, elementsArray[middleOfRange]) > 0 && Compare(value, elementsArray[upperRangeLimit]) < 0)
+            else if (Compare(value, _elementsArray[middleOfRange]) > 0 && Compare(value, _elementsArray[upperRangeLimit]) < 0)
             {
                 lowerRangeLimit = middleOfRange;
                 return RecursionSearch(lowerRangeLimit, upperRangeLimit, value);
@@ -132,107 +233,6 @@ namespace AlexList
             {
                 return -1;
             }
-        }
-
-        public void Clear()
-        {
-            elementsArray = new T[0];
-        }
-
-        public bool Contains(T value)
-        {
-            if (IndexOf(value) == -1)
-                return false;
-            else
-                return true;
-        }
-
-        public int FindIndex(Predicate<T> predicate)
-        {
-            for (int counter = 0; counter < elementsArray.Length - 1; counter++)
-            {
-                if (predicate.Invoke(elementsArray[counter]))
-                {
-                    return counter;
-                }
-            }
-
-            return -1;
-        }
-
-        public int FindLastIndex(Predicate<T> predicate)
-        {
-            for (int counter = elementsArray.Length - 1; counter > -1; counter--)
-            {
-                if (predicate.Invoke(elementsArray[counter]))
-                {
-                    return counter;
-                }
-            }
-
-            return -1;
-        }
-
-        public int IndexOf(T value)
-        {
-            for (int counter = 0; counter < elementsArray.Length - 1; counter++)
-            {
-                if (Compare(value, elementsArray[counter]) == 0)
-                {
-                    return counter;
-                }
-            }
-
-            return -1;
-        }
-
-        public void Insert(T value, int index)
-        {
-            ArrayResize(elementsArray.Length + 1);
-
-            for (int counter = elementsArray.Length - 2; counter >= index; counter--)
-            {
-                elementsArray[counter + 1] = elementsArray[counter];
-            }
-
-            elementsArray[index] = value;
-        }
-
-        public void Remove(T value)
-        {
-            int index = IndexOf(value);
-            RemoveAt(index);
-        }
-
-        public void RemoveAt(int index)
-        {
-            for (int counter = index; counter < elementsArray.Length - 1; counter++)
-            {
-                elementsArray[counter] = elementsArray[counter + 1];
-            }
-
-            ArrayResize(elementsArray.Length - 1);
-        }
-
-        public void Sort()
-        {
-            bool arrayIsNotSorted;
-
-            do
-            {
-                arrayIsNotSorted = false;
-
-                for (int counter = 0; counter < elementsArray.Length - 1; counter++)
-                {
-                    if (Compare(elementsArray[counter], elementsArray[counter + 1]) > 0)
-                    {
-                        Insert(elementsArray[counter + 1], counter);
-                        RemoveAt(counter + 2);
-                        arrayIsNotSorted = true;
-                    }
-                }
-            }
-            while (arrayIsNotSorted == true);
         }
     }
 }
