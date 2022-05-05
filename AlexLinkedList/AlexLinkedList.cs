96c0bdd66dd2f1697908fc1ddd06f9d7d85de553
alex95cooper <lex95bond@gmail.com>
2022-05-05 18:22:21 +0300

    Added class LinkedNode and proops: Count, First, Last

--- AlexLinkedList/AlexLinkedList.cs
+++ AlexLinkedList/AlexLinkedList.cs
@@ -4,7 +4,14 @@
     {
         public AlexLinkedList()
         {
-                
+            Count = 0;
+            First = null;
+            Last = null;
         }
+
+        public int Count { get; set; }
+        public LinkedNode<T> First { get; set; }
+        public LinkedNode<T> Last { get; set; }
+
     }
 }
---
+++ AlexLinkedList/LinkedNode.cs
@@ -0,0 +1,10 @@
+﻿namespace AlexLinkedList
+{
+    public class LinkedNode<T>
+    {
+        public int Index { get; set; }
+        public T Value { get; set; }        
+        public LinkedNode<T> PreviousNode { get; set; }
+        public LinkedNode<T> NextNode { get; set; }
+    }
+}
